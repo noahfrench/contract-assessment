40ec09662a3d122d042dbff01616491c21347d3e
Noah French <noahjfrench@gmail.com>
2018-06-04 13:53:29 -0400

    Submitted contracts are now pushed to firebase. The state that contains the list of forms is now dynamically updated from the firebase database. The currently submitted contracts are displayed.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import Form from "./Form.js";
+import firebase from "./firebase.js";
 
 class App extends Component {
   constructor(props) {
@@ -9,7 +10,7 @@ class App extends Component {
       name: "",
       company: "",
       details: "",
-      submittedContracts: []
+      submittedForms: []
     };
   }
 
@@ -19,11 +20,48 @@ class App extends Component {
     });
   };
 
+  componentDidMount() {
+    const formsRef = firebase.database().ref("submittedForms");
+    formsRef.on("value", snapshot => {
+      let forms = snapshot.val();
+      let newState = [];
+      for (let form in forms) {
+        let newForm = {
+          name: forms[form].name,
+          company: forms[form].company,
+          details: forms[form].details
+        };
+        newState.push(newForm);
+      }
+      this.setState({
+        submittedForms: newState
+      });
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <Form updateParentState={this.updateField} />
+        <Form
+          updateParentState={this.updateField}
+          currentName={this.state.name}
+          currentCompany={this.state.company}
+          currentDetails={this.state.details}
+        />
         <br />
+        Submitted Contracts:
+        <br />
+        <br />
+        {this.state.submittedForms.map(form => {
+          return (
+            <div>
+              <div>Name: {form.name}</div>
+              <div>Company: {form.company}</div>
+              <div>Contract Details: {form.details}</div>
+              <br />
+            </div>
+          );
+        })}
       </div>
     );
   }
